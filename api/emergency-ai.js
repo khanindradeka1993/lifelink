122caee822457b5e8fa51168e6bc45982da4958b
khanindradeka1993 <118038308+khanindradeka1993@users.noreply.github.com>
2026-07-04 00:56:38 +0530

    Update emergency-ai.js

--- api/emergency-ai.js
+++ api/emergency-ai.js
@@ -6,12 +6,15 @@ export default async function handler(req, res) {
   const { question } = req.body;
 
   try {
+  console.log("Key exists:", !!process.env.OPENROUTER_API_KEY);  
     const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
-        "Content-Type": "application/json"
-      },
+  "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
+  "Content-Type": "application/json",
+  "HTTP-Referer": "https://lifelink-q73z.vercel.app",
+  "X-Title": "LifeLink"
+},
       body: JSON.stringify({
         model: "meta-llama/llama-3.1-8b-instruct:free",
         messages: [
